0804659986a18a65124eabb2ebd8d8723cc5bb47
David Rousselie <david.rousselie@happycoders.org>
2013-01-16 09:30:22 +0100

    Create a link to node_modules instead of a copy

--- client/node/install_npm_modules.sh
+++ client/node/install_npm_modules.sh
@@ -30,10 +30,8 @@ TARGET_NAME=$(generate_npm_modules)
 echo "Installing npm modules $LOCAL_NPM_MODULES_HASH"
 
 finalize() {
-  #save the file tree in a tmp folder for debug
-  ls -alR $NVM_DIR/v$LOCAL_NODE_VERSION/modules/$LOCAL_NPM_MODULES_HASH > "/tmp/$LOCAL_NPM_MODULES_HASH"
-  sync
-  cp -r $NVM_DIR/v$LOCAL_NODE_VERSION/modules/$LOCAL_NPM_MODULES_HASH node_modules
+  rm -rf node_modules
+  ln -s $NVM_DIR/v$LOCAL_NODE_VERSION/modules/$LOCAL_NPM_MODULES_HASH node_modules
 }
 
 if [ -d $NVM_DIR/v$LOCAL_NODE_VERSION/modules/$LOCAL_NPM_MODULES_HASH ]; then
