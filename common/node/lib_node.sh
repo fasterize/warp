7d9065eeb7c5ebc6257b69fe67c2cbd84220cb04
David Rousselie <david.rousselie@happycoders.org>
2014-07-21 15:26:10 +0200

    Fix node version detection

--- common/node/lib_node.sh
+++ common/node/lib_node.sh
@@ -11,7 +11,7 @@ load_node_config() {
   else
     if [ -f package.json ]; then
       cat package.json | $WARP_HOME/common/json.sh | grep '\["engines","node"\]' | awk '{print $2}' | perl -pe 's/"//g' > .node_version
-      LOCAL_NODE_VERSION=`cat .node_version | grep -e '[[:digit:]]\.[[:digit:]]\.[[:digit:]]'`
+      LOCAL_NODE_VERSION=`cat .node_version | grep -e '[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+'`
       if [ "$LOCAL_NODE_VERSION" = "" ]; then
         echo "Wrong node version found in package.json : `cat .node_version`"
         rm .node_version
